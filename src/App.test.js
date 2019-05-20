f55630c639b24bae7a3f81e8fd5c9883c8c7beda
filippoimarisio <filippo.imarisio@gmail.com>
2019-05-20 18:05:19 +0200

    final

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { shallow } from 'enzyme';
+import NewsCard from './components/NewsCard';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');

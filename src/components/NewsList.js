c09f6f2830506bf81b982550d236dea7f0c176aa
filippoimarisio <filippo.imarisio@gmail.com>
2019-05-20 15:21:21 +0200

    awaits the fetched data with a loader

--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
 import {fetchNews} from '../actions/fetchNews'
+import {
+  CircularProgress,
+  Typography,
+} from '@material-ui/core'
 
 
 class NewsList extends Component {
@@ -11,19 +15,36 @@ class NewsList extends Component {
 
   render() {
 
-    const news = this.props.news
+    if (!this.props.news.articles) {
+      return (
+        <div>
+          <CircularProgress />{' '}
+          <Typography>Loading today's stories...</Typography>
+        </div>
+      )
+    }
+
+    const articles = this.props.news.articles
 
     return (
       <div>
-
+        <ul>
+          { articles.map(article => {
+            return (
+              <li key={article.url}>
+                {article.title}
+              </li>
+            )
+          })}
+        </ul>
       </div>
     );
   }
 }
 
 const mapStateToProps = ({news}) => {
-  console.log(news)
-  return news
+  console.log('NEWS',news.articles)
+  return {news}
 }
 
 export default connect(mapStateToProps, {fetchNews})(NewsList);
